aa46404b1fef0f4bfbd93fb86487b57735e19270
Kevin Nguyen <Kevin.Nguyen0509@gmail.com>
2023-10-30 21:19:41 -0700

    Initial create

---
+++ KevinsCorePortfolio/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using KevinsCorePortfolio.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace KevinsCorePortfolio.Controllers
+{
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
+
+		public HomeController(ILogger<HomeController> logger)
+		{
+			_logger = logger;
+		}
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult Portfolio()
+		{
+			return View();
+		}
+
+	}
+}
